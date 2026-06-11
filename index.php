d0adb1c0dcd659c4a2b315115f6a956210760f3d
rykialviansyahh-png <rykialviansyahh@gmail.com>
2026-06-11 19:45:22 +0700

    Membuat Form sederhana

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Belajar PHP</title>
-    </head>
-    <body>
-        <?php
-            echo "Saya sedang belajar PHP<br>";
-            echo "<p>Belajar PHP hingga jadi master</p>";
-        ?>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Sederhana</title>
+</head>
+<body>
+
+<form>
+    <div>
+        <label>Nama</label><br>
+        <input name="nama" type="text" placeholder="Masukkan nama">
+    </div>
+
+    <div>
+        <label>Alamat</label><br>
+        <input name="alamat" type="text" placeholder="Masukkan alamat">
+    </div>
+
+    <div>
+        <button>Submit</button>
+    </div>
+</form>
+
+</body>
 </html>
