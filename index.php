73566d5e33c92bdda17cb158d32d158554308b74
rykialviansyahh-png <rykialviansyahh@gmail.com>
2026-06-11 19:31:17 +0700

    Menggabungkan HTML dan PHP

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Belajar PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "Saya sedang belajar PHP<br>";
+            echo "<p>Belajar PHP hingga jadi master</p>";
+        ?>
+    </body>
+</html>
